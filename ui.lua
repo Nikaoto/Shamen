3a050a3dd321d8910117e520de47c21a9cf766cb
zoolander <guga.katsiashvili@gmail.com>
2017-10-27 06:51:04 +0400

    world's gayiest ui module is ready

--- ui.lua
+++ ui.lua
@@ -3,30 +3,35 @@ package.path = package.path .. ";../?.lua"
 ui = {}
 
 -- circles position
-ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X = 20
-ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_Y = 20
-ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X = screenWidth - 20
-ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_Y = 20
+ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X = 60
+ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_Y = 60
+ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X = screenWidth - 60
+ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_Y = 60
+ui.avatarRadius = 50
 
 -- mana and health bar positions
-ui.HEALTH_BAR_HEIGHT = 10
-ui.MANA_BAR_HEIGHT = 5
--- height , width
-ui.PLAYER_HEALTH_BAR_WIDTH = 150
-ui.PLAYER_MANA_BAR_WIDTH = 70
-ui.PLAYER_TWO_HEALTH_BAR_WIDTH = 150
-ui.PLAYER_TWO_MANA_BAR_WIDTH = 70
--- Player 1 health
-ui.HEALTH_BAR_PLAYER_X = ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X + 20
-ui.HEALTH_BAR_PLAYER_Y = 10
--- Player 2 health
-ui.HEALTH_BAR_PLAYER_TWO_X = ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X - ui.PLAYER_TWO_HEALTH_BAR_WIDTH
-ui.HEALTH_BAR_PLAYER_TWO_Y = 10
+ui.HEALTH_BAR_HEIGHT = 20
+ui.MANA_BAR_HEIGHT = 10
 
-ui.MANA_BAR_PLAYER_X = ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X + 20
-ui.MANA_BAR_PLAYER_Y = 19
-ui.MANA_BAR_PLAYER_TWO_X = screenWidth - 110
-ui.MANA_BAR_PLAYER_TWO_Y = 19
+-- height , width
+ui.playerOneHealthBarWidth = 200
+ui.playerTwoHealthBarWidth = 200
+
+ui.playerOneManaBarWidth = 95
+ui.playerTwoManaBarWidth = 95
+
+-- Player 1 health
+ui.HEALTH_BAR_PLAYER_X = ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X
+ui.HEALTH_BAR_PLAYER_Y = 25
+
+-- Player 2 health
+ui.HEALTH_BAR_PLAYER_TWO_X = ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X - ui.playerTwoHealthBarWidth
+ui.HEALTH_BAR_PLAYER_TWO_Y = 25
+
+ui.MANA_BAR_PLAYER_X = ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X + ( ui.avatarRadius - 2 )
+ui.MANA_BAR_PLAYER_Y = 45
+ui.MANA_BAR_PLAYER_TWO_X = ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X - ui.playerTwoManaBarWidth - (ui.avatarRadius - 2)
+ui.MANA_BAR_PLAYER_TWO_Y = 45
 
 --colors
 ui.AVATAR_CIRCLE_COLOR = {}
@@ -36,43 +41,37 @@ ui.MANA_BAR_COLOR = {47, 135, 224}
 function ui:draw()
   --drawing hp bar
   love.graphics.setColor(ui.HEALTH_BAR_COLOR)
-  love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_X, ui.HEALTH_BAR_PLAYER_Y, ui.PLAYER_HEALTH_BAR_WIDTH, ui.HEALTH_BAR_HEIGHT)
-  love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_TWO_X, ui.HEALTH_BAR_PLAYER_TWO_Y, ui.PLAYER_TWO_HEALTH_BAR_WIDTH,
+  love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_X, ui.HEALTH_BAR_PLAYER_Y, ui.playerOneHealthBarWidth, ui.HEALTH_BAR_HEIGHT)
+  love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_TWO_X, ui.HEALTH_BAR_PLAYER_TWO_Y, ui.playerTwoHealthBarWidth,
                       ui.HEALTH_BAR_HEIGHT)
 
   --drawing mana bar
   love.graphics.setColor(ui.MANA_BAR_COLOR)
-  love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_X, ui.MANA_BAR_PLAYER_Y, ui.PLAYER_MANA_BAR_WIDTH, ui.MANA_BAR_HEIGHT)
-  love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_TWO_X, ui.MANA_BAR_PLAYER_TWO_Y, ui.PLAYER_TWO_MANA_BAR_WIDTH, ui.MANA_BAR_HEIGHT)
+  love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_X, ui.MANA_BAR_PLAYER_Y, ui.playerOneManaBarWidth, ui.MANA_BAR_HEIGHT)
+  love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_TWO_X, ui.MANA_BAR_PLAYER_TWO_Y, ui.playerTwoManaBarWidth, ui.MANA_BAR_HEIGHT)
 
   --drawing circles
-  love.graphics.circle("fill", ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X, ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_Y, 30)
-  love.graphics.circle("fill", ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X, ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_Y, 30)
+  love.graphics.circle("fill", ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X, ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_Y, ui.avatarRadius)
+  love.graphics.circle("fill", ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X, ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_Y, ui.avatarRadius)
   -- back to DEFAULT
   love.graphics.setColor(255, 255, 255, 255)
 end
 
 function ui:update(player, player2)
-  setPlayerOneHpMp(player.hp, player.mp)
-  setPlayerTwoHpMp(player2.hp, player2.mp)
+  updatePlayerOne(player)
+  updatePlayerTwo(player2)
 end
 
-function setPlayerOneHpMp(hp, mp)
-  --
-  --ui.HEALTH_BAR_PLAYER_X = ui.HEALTH_BAR_PLAYER_X - (ui.PLAYER_HEALTH_BAR_WIDTH - hp * 1.5)
-  ui.PLAYER_HEALTH_BAR_WIDTH = hp * 1.5
-  --
-  --ui.MANA_BAR_PLAYER_X = ui.MANA_BAR_PLAYER_X - (ui.PLAYER_MANA_BAR_WIDTH - mp * 0.7)
-  ui.PLAYER_MANA_BAR_WIDTH = mp * 0.7
+function updatePlayerOne(playerOne)
+  ui.playerOneHealthBarWidth = playerOne.hp * 2
+  ui.playerOneManaBarWidth = playerOne.mp * 0.95
 end
 
-function setPlayerTwoHpMp(hp, mp)
-  --
-  ui.HEALTH_BAR_PLAYER_TWO_X = ui.HEALTH_BAR_PLAYER_TWO_X + (ui.PLAYER_TWO_HEALTH_BAR_WIDTH - hp * 1.5)
-  ui.PLAYER_TWO_HEALTH_BAR_WIDTH = hp * 1.5
-  --
-  ui.MANA_BAR_PLAYER_TWO_X = ui.MANA_BAR_PLAYER_TWO_X + (ui.PLAYER_TWO_MANA_BAR_WIDTH - mp * 0.7)
-  ui.PLAYER_TWO_MANA_BAR_WIDTH = mp * 0.7
+function updatePlayerTwo(playerTwo)
+  ui.HEALTH_BAR_PLAYER_TWO_X = ui.HEALTH_BAR_PLAYER_TWO_X + (ui.playerTwoHealthBarWidth - playerTwo.hp * 2)
+  ui.playerTwoHealthBarWidth = playerTwo.hp * 2
+  ui.MANA_BAR_PLAYER_TWO_X = ui.MANA_BAR_PLAYER_TWO_X + (ui.playerTwoManaBarWidth - playerTwo.mp * 0.95)
+  ui.playerTwoManaBarWidth = playerTwo.mp * 0.95
 end
 
 -- function ui:setMana(player, mana)
