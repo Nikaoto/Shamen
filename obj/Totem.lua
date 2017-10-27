f86b2e247826fd5fbd4a722cdb9feedd86db0c59
Nikaoto <nikaoto@gmail.com>
2017-10-27 07:40:14 +0400

    fix z axis rendering

--- obj/Totem.lua
+++ obj/Totem.lua
@@ -7,7 +7,7 @@ require "lib/deep"
 Totem = Object:extend()
 
 Totem.WIDTH = 50
-Totem.HEIGHT = 50
+Totem.HEIGHT = 70
 Totem.DEFAULT_AREAL_Z = 2
 Totem.DEFAULT_AREAL_SIZE = 60
 Totem.DEFAULT_COLOR = {0, 255, 255}
@@ -21,31 +21,30 @@ function Totem:new(coords, areal, color)
   self.width = Totem.WIDTH
   self.height = Totem.HEIGHT
 
-  self.ox = coords.x
-  self.oy = coords.y
-  coords.y = coords.y - self.height / 2
-  coords.x = coords.x - self.width / 2
+  self.ox = self.width / 2
+  self.oy = self.height / 2
   self.startY = coords.y - screenHeight
-  self.endY = coords.y
-  self.x , self.y , self.z = coords.x , self.startY , coords.z
-  self.tween = tween.new(Totem.DEFAULT_FALL_TIME, self, { y = coords.y, z = coords.z + self.height}, tween.easing.expoIn)
+  self.endY = coords.y - self.height
+  self.x , self.y , self.z = coords.x - self.ox , self.startY , math.floor(coords.z)
+  self.tween = tween.new(Totem.DEFAULT_FALL_TIME, self, { y = self.endY }, tween.easing.expoIn)
   return self
 end
 
 function Totem:log()
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print("x = " .. self.x .. ", y = ".. self.y .. ", z = ")
+  deep:print("x = " .. self.x .. ", y = ".. self.y .. ", z = " .. self.z, self.x, self.y, self.z)
 end
 
 function Totem:draw()
-  deep:rectangleC(self.color, "fill", self.x, self.y, math.floor(self.z), self.width, self.height)
+  deep:rectangleC(self.color, "fill", self.x, self.y, self.z, self.width, self.height)
   if self.complete then
-    deep:ellipseC(self.color, "line", self.ox, self.oy, Totem.DEFAULT_AREAL_Z, self.areal, self.areal / 2)
+    deep:ellipseC(self.color, "line", self.x + self.ox, self.y + self.oy, Totem.DEFAULT_AREAL_Z, self.areal, self.areal / 2)
   end
+  self:log()
 end
 
 function Totem:update(dt)
   self.complete = self.tween:update(dt)
+  self.z = math.floor(self.y + self.height)
   if self.complete and not self.shook then
     screen:setShake(Totem.DEFAULT_SHAKE_AMOUNT)
     self.shook = true
