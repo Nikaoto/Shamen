17b6a9c4fe7d36bc72dce04d0b4c0c3672cd8122
Nikaoto <nikaoto@gmail.com>
2017-10-28 13:55:39 +0400

    add outlines to ui

--- ui.lua
+++ ui.lua
@@ -31,21 +31,43 @@ ui.HEALTH_BAR_PLAYER_TWO_X = ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X - ui.player
 ui.HEALTH_BAR_PLAYER_TWO_Y = 25
 
 ui.MANA_BAR_PLAYER_X = ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X + ( ui.avatarRadius - 4 )
-ui.MANA_BAR_PLAYER_Y = 45
+ui.MANA_BAR_PLAYER_Y = 50
 ui.MANA_BAR_PLAYER_TWO_X = ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X - ui.playerTwoManaBarWidth - (ui.avatarRadius - 4)
-ui.MANA_BAR_PLAYER_TWO_Y = 45
+ui.MANA_BAR_PLAYER_TWO_Y = 50
 
 --colors
 ui.AVATAR_CIRCLE_COLOR = {}
 ui.HEALTH_BAR_COLOR = {255, 5, 5}
 ui.MANA_BAR_COLOR = {47, 135, 224}
+ui.CHARACTER_CIRCLE_COLOR = {47, 135, 224}
+ui.OUTLINE_COLOR = {0, 0, 0}
+ui.OUTLINE_WIDTH = 5
 
 function ui:draw()
+  -- drawing outlines
+  love.graphics.setColor(ui.OUTLINE_COLOR)
+  local w = ui.OUTLINE_WIDTH
+  --hp
+  love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_X - w, ui.HEALTH_BAR_PLAYER_Y - w,
+   ui.playerOneHealthBarWidth + 2*w, ui.HEALTH_BAR_HEIGHT + 2*w, 10 + w)
+  love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_TWO_X - w, ui.HEALTH_BAR_PLAYER_TWO_Y - w, 
+    ui.playerTwoHealthBarWidth + 2*w, ui.HEALTH_BAR_HEIGHT + 2*w, 10 + w)
+  --mana
+  love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_X - w, ui.MANA_BAR_PLAYER_Y - w, 
+    ui.playerOneManaBarWidth + 2*w, ui.MANA_BAR_HEIGHT + 2*w, 5 + w)
+  love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_TWO_X - w, ui.MANA_BAR_PLAYER_TWO_Y - w, 
+    ui.playerTwoManaBarWidth + 2*w, ui.MANA_BAR_HEIGHT + 2*w, 5 + w)
+  --characters
+  love.graphics.circle("fill", ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X, 
+    ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_Y, ui.avatarRadius + w)
+  love.graphics.circle("fill", ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X, 
+    ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_Y, ui.avatarRadius + w)
+
   --drawing hp bar
   love.graphics.setColor(ui.HEALTH_BAR_COLOR)
   love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_X, ui.HEALTH_BAR_PLAYER_Y, ui.playerOneHealthBarWidth, ui.HEALTH_BAR_HEIGHT,10)
   love.graphics.rectangle("fill", ui.HEALTH_BAR_PLAYER_TWO_X, ui.HEALTH_BAR_PLAYER_TWO_Y, ui.playerTwoHealthBarWidth,
-                      ui.HEALTH_BAR_HEIGHT,10)
+                      ui.HEALTH_BAR_HEIGHT, 10)
 
   --drawing mana bar
   love.graphics.setColor(ui.MANA_BAR_COLOR)
@@ -53,8 +75,10 @@ function ui:draw()
   love.graphics.rectangle("fill", ui.MANA_BAR_PLAYER_TWO_X, ui.MANA_BAR_PLAYER_TWO_Y, ui.playerTwoManaBarWidth, ui.MANA_BAR_HEIGHT,5)
 
   --drawing circles
+  love.graphics.setColor(ui.CHARACTER_CIRCLE_COLOR)
   love.graphics.circle("fill", ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_X, ui.DEFAULT_PLAYER_ONE_AVATAR_POSITION_Y, ui.avatarRadius)
   love.graphics.circle("fill", ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_X, ui.DEFAULT_PLAYER_TWO_AVATAR_POSITION_Y, ui.avatarRadius)
+
   -- back to DEFAULT
   love.graphics.setColor(255, 255, 255, 255)
 end
